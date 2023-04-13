0de97a4cd4633ff34f49ddfe877931bdbdf0b913
ansilin1903 <111332523+ansilin1903@users.noreply.github.com>
2023-04-13 14:21:34 +0530

    Update cipher.php

--- cipher.php
+++ cipher.php
@@ -1,12 +1,24 @@
+
 <?php
     $plaintext="HELLO";
-    $key=4;
+    $key=8;
     $result="";
+    $result1="";
     for($i=0;$i<strlen($plaintext);$i++){
         if (ctype_upper($plaintext[$i])){
-            $result = $result.chr((ord($plaintext[$i]) + $key - 65) % 26 + 65);
+            $result = $result.chr((ord($plaintext[$i]) + $key - 65)%26 + 65);
         }
     }
-    echo $result;
+    for($i=0;$i<strlen($plaintext);$i++){
+        if (ctype_upper($plaintext[$i])){
+            $result1 = $result1.chr((ord($plaintext[$i]) - $key - 65) % 26 + 65);
+        }
+    }
+    echo "This is the plaintext: $plaintext";
+    echo "</br>";
+    echo "This is the key: $key";
+    echo "</br>";
+    echo "This is the encrypted:$result";
+    echo "</br>";
+    echo "This is the decrypted:$result1";
 ?>
-
